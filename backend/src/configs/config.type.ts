5af0965e6638264131a58043c1d941a08ae0ba05
NadiyPro <siroviyn13@gmail.com>
2025-01-30 21:00:00 +0200

    create

---
+++ backend/src/configs/config.type.ts
@@ -0,0 +1,51 @@
+export type Config = {
+  app: AppConfig;
+  database: DatabaseConfig;
+  redis: RedisConfig;
+  aws: AwsConfig;
+  // sentry: SentryConfig;
+  jwt: JwtConfig;
+  email: EmailConfig;
+};
+
+export type AppConfig = {
+  port: number;
+  host: string;
+};
+export type DatabaseConfig = {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  name: string;
+};
+export type RedisConfig = {
+  host: string;
+  port: number;
+  password: string;
+};
+export type AwsConfig = {
+  accessKey: string;
+  secretKey: string;
+  bucketAvatar: string;
+  bucketCars: string;
+  region: string;
+  ACL: string;
+  endpoint: string;
+};
+// export type SentryConfig = {
+//   dsn: string;
+//   env: string;
+//   debug: boolean;
+// };
+export type JwtConfig = {
+  accessSecret: string;
+  accessExpiresIn: number;
+  refreshSecret: string;
+  refreshExpiresIn: number;
+};
+
+export type EmailConfig = {
+  email: string;
+  password: string;
+};
