5c261db328b455de71ad4000e3106755ca9d97d6
NadiyPro <siroviyn13@gmail.com>
2025-02-02 12:09:05 +0200

    create

--- backend/src/modules/auth/decorators/current_user.decorator.ts
+++ backend/src/modules/auth/decorators/current_user.decorator.ts
@@ -1,8 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+import { IUserData } from '../models/interfaces/user_data.interface';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    return request.res.locals.user;
+  (_data: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest<Request>(); // Додаємо типізацію
+    return request.res.locals.user as IUserData; // Перевіряємо наявність `locals`
   },
 );
