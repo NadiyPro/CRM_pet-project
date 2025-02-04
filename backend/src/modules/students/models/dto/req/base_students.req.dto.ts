2c5858bee684969e5df35ead4d4ceffc9514753b
NadiyPro <siroviyn13@gmail.com>
2025-02-04 20:09:40 +0200

    create

--- backend/src/modules/students/models/dto/req/base_students.req.dto.ts
+++ backend/src/modules/students/models/dto/req/base_students.req.dto.ts
@@ -1,6 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
-import { IsNumber, IsString, Length, Matches, Max, Min } from 'class-validator';
+import {
+  IsEnum,
+  IsNumber,
+  IsString,
+  Length,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
 import { TransformHelper } from '../../../../../common/helpers/transform.helper';
 import { CourseEnum } from '../../../../../infrastructure/mySQL/entities/enums/course.enum';
 import { CourseFormatEnum } from '../../../../../infrastructure/mySQL/entities/enums/courseFormat.enum';
@@ -47,24 +55,33 @@ export class BaseStudentsReqDto {
   @Length(2, 5)
   @Transform(({ value }) => TransformHelper.trim({ value: value as string }))
   @Type(() => String)
+  @IsEnum(CourseEnum, {
+    message: 'course can be one of: FS, QACX, JCX, JSCX, FE, PCX',
+  })
   course: CourseEnum | null;
 
-  @IsString()
   @Length(5, 10)
   @Transform(({ value }) => TransformHelper.trim({ value: value as string }))
   @Type(() => String)
+  @IsEnum(CourseFormatEnum, {
+    message: 'course_format can be one of: static, online',
+  })
   course_format: CourseFormatEnum | null;
 
-  @IsString()
   @Length(3, 15)
   @Transform(({ value }) => TransformHelper.trim({ value: value as string }))
   @Type(() => String)
+  @IsEnum(CourseTypeEnum, {
+    message: 'course_type can be one of: pro, minimal, premium, incubator, vip',
+  })
   course_type: CourseTypeEnum | null;
 
-  @IsString()
   @Length(3, 15)
   @Transform(({ value }) => TransformHelper.trim({ value: value as string }))
   @Type(() => String)
+  @IsEnum(StatusEnum, {
+    message: 'status can be one of: In_work, New, Aggre, Disaggre, Dubbing ',
+  })
   status: StatusEnum | null;
 
   @ApiProperty({ example: 100000 })
