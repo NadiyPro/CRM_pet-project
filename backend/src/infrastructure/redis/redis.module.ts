4fe3b8940a38320a7f7eb4e5354786a075ef0745
NadiyPro <siroviyn13@gmail.com>
2025-02-17 15:28:36 +0200

    create

--- backend/src/infrastructure/redis/redis.module.ts
+++ backend/src/infrastructure/redis/redis.module.ts
@@ -1,3 +1,40 @@
+// import { Module } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import Redis from 'ioredis';
+//
+// import { Config, RedisConfig } from '../../configs/config.type';
+// import { RedisService } from './services/redis.service';
+//
+// @Module({
+//   providers: [
+//     {
+//       provide: 'REDIS_CLIENT',
+//       //  провайдер реєструється під назвою 'REDIS'.
+//       //  У будь-якому іншому місці програми його можна буде підключити через токен 'REDIS'
+//       useFactory: (configService: ConfigService<Config>) => {
+//         // useFactory - функція, яка виконує логіку для налаштування підключення до БД.
+//         // Вона отримує ConfigService як залежність і використовує його для отримання налаштувань БД.
+//         const config = configService.get<RedisConfig>('redis');
+//         // конфігурація завантажується з ConfigService,
+//         // з файлу configuration по ключу 'redis'
+//         return new Redis({
+//           port: config.port,
+//           host: config.host,
+//           password: config.password,
+//         });
+//       },
+//       inject: [ConfigService],
+//       // Вказується масив сервісів, які інжектуються (підкидується) в useFactory
+//     },
+//     RedisService,
+//     //  провайдер RedisService зв'язаний з 'REDIS_CLIENT'
+//     // розміщений тут він нам дає можливість стукатись зовні, тобто далі викликати експорт з ним
+//   ],
+//   exports: [RedisService],
+// }) // підключення до Redis через бібліотеку ioredis
+// // providers - визначає, які провайдери (сервіси або інші інстанції)
+// // будуть доступні у цьому модулі.
+// export class RedisModule {}
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
@@ -9,14 +46,14 @@ import { RedisService } from './services/redis.service';
   providers: [
     {
       provide: 'REDIS_CLIENT',
-      //  провайдер реєструється під назвою 'REDIS'.
-      //  У будь-якому іншому місці програми його можна буде підключити через токен 'REDIS'
       useFactory: (configService: ConfigService<Config>) => {
-        // useFactory - функція, яка виконує логіку для налаштування підключення до БД.
-        // Вона отримує ConfigService як залежність і використовує його для отримання налаштувань БД.
         const config = configService.get<RedisConfig>('redis');
-        // конфігурація завантажується з ConfigService,
-        // з файлу configuration по ключу 'redis'
+        if (!config) {
+          throw new Error('Redis configuration is missing');
+        }
+
+        console.log(`Connecting to Redis at ${config.host}:${config.port}`);
+
         return new Redis({
           port: config.port,
           host: config.host,
@@ -24,14 +61,9 @@ import { RedisService } from './services/redis.service';
         });
       },
       inject: [ConfigService],
-      // Вказується масив сервісів, які інжектуються (підкидується) в useFactory
     },
     RedisService,
-    //  провайдер RedisService зв'язаний з 'REDIS_CLIENT'
-    // розміщений тут він нам дає можливість стукатись зовні, тобто далі викликати експорт з ним
   ],
   exports: [RedisService],
-}) // підключення до Redis через бібліотеку ioredis
-// providers - визначає, які провайдери (сервіси або інші інстанції)
-// будуть доступні у цьому модулі.
+})
 export class RedisModule {}
