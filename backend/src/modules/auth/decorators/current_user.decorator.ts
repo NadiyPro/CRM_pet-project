b2e7af1af05fce52136295fdc2b6fbb7ef97304c
NadiyPro <siroviyn13@gmail.com>
2025-03-24 23:51:00 +0200

    create

--- backend/src/modules/auth/decorators/current_user.decorator.ts
+++ backend/src/modules/auth/decorators/current_user.decorator.ts
@@ -5,6 +5,6 @@ import { IUserData } from '../models/interfaces/user_data.interface';
 export const CurrentUser = createParamDecorator(
   (_data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest<Request>(); // Додаємо типізацію
-    return request.res.locals.user as IUserData; // Перевіряємо наявність `locals`
+    return request.res.locals.user as IUserData;
   },
 );
