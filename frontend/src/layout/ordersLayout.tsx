068445714ec376b33127a5aec4a174663f258d2d
NadiyPro <siroviyn13@gmail.com>
2025-04-24 22:19:06 +0300

    create

--- frontend/src/layout/ordersLayout.tsx
+++ frontend/src/layout/ordersLayout.tsx
@@ -1,5 +1,4 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../redux/store';
+import { Outlet } from 'react-router-dom';
 import LogOutComponent from '../components/ordersLayoutComponent/logOut.component';
 import AdminPanelComponent from '../components/ordersLayoutComponent/adminPanel.component';
 import OrdersPanelComponent from '../components/ordersLayoutComponent/ordersPanel.component';
