13a5855851b1fe88d91e5c773944b59394cc30de
NadiyPro <siroviyn13@gmail.com>
2025-05-05 12:42:18 +0300

    create

--- frontend/src/components/ordersComponents/messagesOrderId.component.tsx
+++ frontend/src/components/ordersComponents/messagesOrderId.component.tsx
@@ -1,12 +1,25 @@
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { orderAction } from '../../redux/slices/orderSlice';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import createMessageValidator from '../../validator/createMessage.validator';
+import { CreateMessageDto } from '../../module/createMessage.dto';
 
 const MessagesOrderIdComponent = () => {
+  const {handleSubmit, register, reset, formState: {isValid}} = useForm<CreateMessageDto>({mode: 'all', resolver: joiResolver(createMessageValidator)})
   const { messagesOrderId, findOneOrder } = useAppSelector((state) => state.orderStore);
   const dispatch = useAppDispatch();
 
   const handleCloseMessagesOrderId = () => {
       dispatch(orderAction.setCloseMessagesOrderId());
+    }
+
+    const handleCreateMessage = (dataMessage: CreateMessageDto ) => {
+    if(findOneOrder.id !== null){
+      let orderId = findOneOrder.id;
+      dispatch(orderAction.loadCreateMessage({orderId, dataMessage }))
+      reset();
+    }
     }
 
   return(
@@ -20,10 +33,10 @@ const MessagesOrderIdComponent = () => {
           <div>{value.messages}</div>
           <div>{value.manager} {value.created_at}</div>
         </div>)}
-        <form>
-        <label></label>
-          <input />
-          <button>SUBMIT</button>
+        <form onSubmit={handleSubmit(handleCreateMessage)}>
+        <label htmlFor={'messages'}>Create message:</label>
+          <input type={'text'} {...register('messages')}/>
+          <button type={'submit'} disabled={!isValid}>SUBMIT</button>
         </form>
         <button onClick={handleCloseMessagesOrderId}>EXIT</button>
       </div>
