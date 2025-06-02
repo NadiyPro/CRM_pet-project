e926599d7365353e4a3a169a60dd873b750b3d35
NadiyPro <siroviyn13@gmail.com>
2025-06-02 20:55:10 +0300

    create

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/global.scss';
+import './styles/styles.scss';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
