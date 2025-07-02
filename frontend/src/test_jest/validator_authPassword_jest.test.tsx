b57f304123872b22e6a73fb2841d37135e2a471c
NadiyPro <siroviyn13@gmail.com>
2025-07-02 19:04:29 +0300

    create

--- frontend/src/test_jest/validator_authPassword_jest.test.tsx
+++ frontend/src/test_jest/validator_authPassword_jest.test.tsx
@@ -21,7 +21,7 @@ describe('authPasswordValidator', () => {
     expect(result.error?.details[0].message).toBe('Пароль повинен містити принаймні одну літеру, одну цифру та один спеціальний символ, і бути не менше 8 символів');
   })
 
-  test('no valid password', () => {
+  test('no valid confirm_password', () => {
     const result = authPasswordValidator.validate({
       password: 'Password111',
       confirm_password: 'password111!'
