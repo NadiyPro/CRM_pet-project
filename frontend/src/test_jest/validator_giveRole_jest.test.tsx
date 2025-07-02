80060bc8e4b8b953bb0d17a3e2ac72cc0f4afdcb
NadiyPro <siroviyn13@gmail.com>
2025-07-02 16:14:59 +0300

    create

---
+++ frontend/src/test_jest/validator_giveRole_jest.test.tsx
@@ -0,0 +1,59 @@
+import 'react';
+import giveRoleValidator from '../validator/giveRole.validator';
+
+
+describe('giveRoleValidator', () => {
+
+  test('valid data', () => {
+    const result = giveRoleValidator.validate({
+      name: 'Nadia',
+      surname: 'Prosianyk',
+      email: 'siroviyn13@gmail.com'
+    });
+    expect(result.error).toBeUndefined();
+  });
+
+  test('no valid name', () => {
+    const result = giveRoleValidator.validate({
+      name: 'Na',
+      surname: 'Prosianyk',
+      email: 'siroviyn13@gmail.com'
+    });
+    // console.log(result);
+
+    expect(result.error?.details[0].message).toBe('min is 3');
+  });
+
+  test('no valid surname', () => {
+    const result = giveRoleValidator.validate({
+      name: 'Nadia',
+      surname: 'Pr',
+      email: 'siroviyn13@gmail.com'
+    });
+    expect(result.error?.details[0].message).toBe('min is 3');
+  });
+
+  test('no valid email', () => {
+    const result = giveRoleValidator.validate({
+      name: 'Nadia',
+      surname: 'Prosianyk',
+      email: 'siroviyn.gmail.com'
+    });
+    expect(result.error?.details[0].message).toBe('Будь ласка, введіть дійсну електронну адресу');
+  });
+})
+export {};
+
+// {
+//   value: { ... }, // дані які перевряємо
+//   error: {
+//     details: [
+//       {
+//         message: 'min is 3',
+//         path: ['name'],
+//         type: 'string.min',
+//         context: { ... }
+//       }
+//     ]
+//   }
+// }
