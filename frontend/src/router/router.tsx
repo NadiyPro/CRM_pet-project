873ff09fed2fff772c82232d279bcd7f5bcc6560
NadiyPro <siroviyn13@gmail.com>
2025-08-19 11:04:00 +0300

    create

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -5,16 +5,49 @@ import MainLayout from '../layout/mainLayout';
 import OrdersAllPage from '../page/ordersAllPage';
 import AdminPage from '../page/adminPage';
 import AuthPasswordPage from '../page/authPasswordPage';
-import AuthRouterComponent from '../components/authComponents/authRouter.component';
 import AuthLoginPage from '../page/authLoginPage';
-import { AuthRedirect } from '../components/authComponents/authRedirect';
+import { AuthRouterComponent } from '../components/authComponents/authRouter.component';
+
+// export const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     errorElement: <ErrorElement />,
+//     children: [
+//       { index: true, element: <Navigate to="/auth/login" replace /> },
+//
+//       {
+//         path: 'auth',
+//         children: [
+//           { index: true, element: <Navigate to="/auth/login" replace /> },
+//           { path: 'login', element: <AuthLoginPage /> },
+//           { path: 'activate/:token', element: <AuthPasswordPage /> },
+//         ],
+//       },
+//
+//       {
+//         element: <AuthRouterComponent />,
+//         children: [
+//           {
+//             element: <MainLayout />,
+//             children: [
+//               { index: true, element: <Navigate to="/orders" replace /> },
+//               { path: 'orders', element: <OrdersAllPage /> },
+//               { path: 'admin', element: <AdminPage /> },
+//             ],
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ]);
 
 export const router = createBrowserRouter([
   {
     path: '/',
     errorElement: <ErrorElement />,
     children: [
-      { index: true, element: <AuthRedirect/> },
+
+      { index: true, element: <Navigate to="/orders" replace /> },
 
       {
         path: 'auth',
