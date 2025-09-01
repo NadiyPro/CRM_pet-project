6f8604cd41c1cbabc5dae10e8b5feee462eefc80
NadiyPro <siroviyn13@gmail.com>
2025-09-01 18:52:53 +0300

    create

--- backend/src/modules/decorators/current_user.decorator.ts
+++ backend/src/modules/decorators/current_user.decorator.ts
@@ -16,47 +16,8 @@ export const CurrentUser = createParamDecorator(
     const response = context.switchToHttp().getResponse<CustomResponse>();
     const user = response.locals?.user;
     if (!user) {
-      // без user → guard не пройшов або щось пішло не так
       throw new UnauthorizedException('User not found in request context');
     }
     return user;
   },
 );
-
-// interface CustomResponse extends Response {
-//   locals: {
-//     user: IUserData;
-//     [key: string]: any;
-//   };
-// }
-//
-// export const CurrentUser = createParamDecorator(
-//   (_data: unknown, context: ExecutionContext): IUserData => {
-//     const response = context.switchToHttp().getResponse<CustomResponse>();
-//     return response.locals.user;
-//   },
-// );
-
-// export const CurrentUser = createParamDecorator(
-//   (_data: unknown, context: ExecutionContext) => {
-//     const request = context.switchToHttp().getRequest<Request>();
-//     return request.res.locals.user as IUserData;
-//   },
-// );
-// context.switchToHttp перемикає загальний ExecutionContext на HTTP-контекст,
-// бо NestJS підтримує кілька транспортів (не лише HTTP),
-// і ми повинні явно вказати, що працюємо з HTTP-запитом
-// дістаємо дані із запиту (токену) і зберігаємо в локалсах
-// interface CustomResponse extends Response {
-//   locals: {
-//     user: IUserData;
-//     [key: string]: any;
-//   };
-// }
-//
-// export const CurrentUser = createParamDecorator(
-//   (_data: unknown, context: ExecutionContext): IUserData => {
-//     const response = context.switchToHttp().getResponse<CustomResponse>();
-//     return response.locals.user;
-//   },
-// );
