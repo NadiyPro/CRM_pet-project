6c0438f1ad69ff531ca70511808d426974bc6324
NadiyPro <siroviyn13@gmail.com>
2025-09-01 23:25:13 +0300

    create

--- backend/src/infrastructure/repository/services/refresh-token.repository.ts
+++ backend/src/infrastructure/repository/services/refresh-token.repository.ts
@@ -2,39 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { RefreshTokenEntity } from '../../mysql/entities/refresh-token.entity';
 
-// @Injectable()
-// export class RefreshTokenRepository extends Repository<RefreshTokenEntity> {
-//   constructor(private readonly dataSource: DataSource) {
-//     super(RefreshTokenEntity, dataSource.manager);
-//   }
-//
-//   // public async isRefreshTokenExist(refreshToken: string): Promise<boolean> {
-//   //   return this.existsBy({ refreshToken });
-//   // }
-//   public async isRefreshTokenExist(refreshToken: string): Promise<boolean> {
-//     const token = await this.findOneBy({ refreshToken });
-//     return !!token;
-//   }
-// }
-// @Injectable()
-// export class RefreshTokenRepository extends Repository<RefreshTokenEntity> {
-//   constructor(private readonly dataSource: DataSource) {
-//     super(RefreshTokenEntity, dataSource.manager);
-//   }
-//
-//   public async isRefreshTokenExist(
-//     userId: string,
-//     deviceId: string,
-//     refreshToken: string,
-//   ): Promise<boolean> {
-//     const token = await this.findOneBy({
-//       user_id: userId,
-//       deviceId,
-//       refreshToken,
-//     });
-//     return !!token;
-//   }
-// }
 @Injectable()
 export class RefreshTokenRepository extends Repository<RefreshTokenEntity> {
   constructor(private readonly dataSource: DataSource) {
