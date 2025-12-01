4d1dfa52fb4cd277dfd318e968a28b90eef62107
NadiyPro <siroviyn13@gmail.com>
2025-12-01 22:24:21 +0200

    Add MySQL dump files

---
+++ backend/src/infrastructure/mysql/dump/group.sql
@@ -0,0 +1,4 @@
+INSERT INTO nadiamysql.`group` (created_at, updated_at, id, group_name) VALUES ('2025-09-24 20:01:26.628541', '2025-09-24 20:01:26.628541', 1, 'march_2024');
+INSERT INTO nadiamysql.`group` (created_at, updated_at, id, group_name) VALUES ('2025-09-24 20:12:53.713303', '2025-09-24 20:12:53.713303', 2, 'june_2025');
+INSERT INTO nadiamysql.`group` (created_at, updated_at, id, group_name) VALUES ('2025-09-24 20:19:31.165003', '2025-09-24 20:19:31.165003', 3, 'october_2024');
+INSERT INTO nadiamysql.`group` (created_at, updated_at, id, group_name) VALUES ('2025-09-24 20:52:32.347629', '2025-09-24 20:52:32.347629', 4, 'december_2024');
---
+++ backend/src/infrastructure/mysql/dump/migrations.sql
@@ -0,0 +1,2 @@
+INSERT INTO nadiamysql.migrations (id, timestamp, name) VALUES (1, 1753623171513, 'AddTable1753623171513');
+INSERT INTO nadiamysql.migrations (id, timestamp, name) VALUES (2, 1756807469736, 'Expefresh1756807469736');
